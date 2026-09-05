ce0bee7904c9eda24c17fc3b14fbecca977db6ff
Ganesh Dabhade <158286559+Ganeshd0003@users.noreply.github.com>
2026-09-05 19:40:36 +0530

    Date Check Constraint added in silver layer data loading

--- scripts/17_Full_Clean_Data_Load_Stored_Procedure_Silver_Layer.sql
+++ scripts/17_Full_Clean_Data_Load_Stored_Procedure_Silver_Layer.sql
@@ -5,6 +5,7 @@ CREATE OR ALTER PROCEDURE silver.full_data_loading
 AS
 BEGIN
     SET NOCOUNT ON;
+
     -- 1)
     PRINT '>> Truncating table : silver.crm_cust_info'
     TRUNCATE TABLE silver.crm_cust_info;
@@ -48,54 +49,56 @@ BEGIN
     ) t
     WHERE Flag = 1 AND t.cst_id IS NOT NULL;
 
+
     -- 2)
     PRINT '>> Truncating table : silver.crm_prd_info'
     TRUNCATE TABLE silver.crm_prd_info;
 
     PRINT '>> Inserting Data into : silver.crm_prd_info'
-    INSERT INTO silver.crm_prd_info 
+    INSERT INTO silver.crm_prd_info
     (
-	    prd_id,
-	    cat_id,
-	    prd_key,
-	    prd_nm,
-	    prd_cost,
-	    prd_line,
-	    prd_start_dt,
-	    prd_end_dt
+        prd_id,
+        cat_id,
+        prd_key,
+        prd_nm,
+        prd_cost,
+        prd_line,
+        prd_start_dt,
+        prd_end_dt
     )
-	    SELECT TOP 1000
-		    prd_id,
-		    REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
-		    SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
-		    prd_nm,
-		    ISNULL(prd_cost,0) AS prd_cost,
-		    CASE UPPER(TRIM(prd_line))
-			    WHEN 'M' THEN 'Mountain'
-			    WHEN 'R' THEN 'Road'
-			    WHEN 'T' THEN 'Touring'
-			    WHEN 'S' THEN 'Other Sales'
-			    ELSE 'n/a'
-		    END AS prd_line,
-		    CAST (prd_start_dt AS DATE) AS prd_state_dt,
-		    CAST(
-		    DATEADD(
-			    DAY,
-			    -1,
-			    LEAD(prd_start_dt) OVER (
-				    PARTITION BY prd_key
-				    ORDER BY prd_start_dt
-			    )
-		    ) AS DATE
-	    ) AS prd_end_dt
-	    FROM bronze.crm_prd_info;
+    SELECT TOP 1000
+        prd_id,
+        REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
+        SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
+        prd_nm,
+        ISNULL(prd_cost,0) AS prd_cost,
+        CASE UPPER(TRIM(prd_line))
+            WHEN 'M' THEN 'Mountain'
+            WHEN 'R' THEN 'Road'
+            WHEN 'T' THEN 'Touring'
+            WHEN 'S' THEN 'Other Sales'
+            ELSE 'n/a'
+        END AS prd_line,
+        CAST (prd_start_dt AS DATE) AS prd_state_dt,
+        CAST(
+            DATEADD(
+                DAY,
+                -1,
+                LEAD(prd_start_dt) OVER (
+                    PARTITION BY prd_key
+                    ORDER BY prd_start_dt
+                )
+            ) AS DATE
+        ) AS prd_end_dt
+    FROM bronze.crm_prd_info;
 
-    --3)
+
+    -- 3)
     PRINT '>> Truncating table : silver.crm_sales_details'
     TRUNCATE TABLE silver.crm_sales_details;
 
     PRINT '>> Inserting Data into : silver.crm_sales_details'
-    INSERT INTO silver.crm_sales_details 
+    INSERT INTO silver.crm_sales_details
     (
         sls_ord_num,
         sls_prd_key,
@@ -107,26 +110,35 @@ BEGIN
         sls_quantity,
         sls_price
     )
-
     (
-	    SELECT sls_ord_num,
-          sls_prd_key,
-          sls_cust_id,
-          TRY_CONVERT(DATE,CAST(sls_order_dt AS varchar(8)),112) AS sls_order_dtt,
-          TRY_CONVERT(DATE,CAST(sls_ship_dt AS VARCHAR(8)),112) AS sls_ship_dtt,
-          TRY_CONVERT(DATE,CAST(sls_due_dt AS VARCHAR(8)),112) AS sls_due_dtt,
-          CASE
-            WHEN sls_sales IS NULL OR sls_sales < 0 OR sls_sales <> (sls_quantity * ABS(sls_price))
+        SELECT
+            sls_ord_num,
+            sls_prd_key,
+            sls_cust_id,
+            TRY_CONVERT(DATE,CAST(sls_order_dt AS varchar(8)),112) AS sls_order_dtt,
+            TRY_CONVERT(DATE,CAST(sls_ship_dt AS VARCHAR(8)),112) AS sls_ship_dtt,
+            TRY_CONVERT(DATE,CAST(sls_due_dt AS VARCHAR(8)),112) AS sls_due_dtt,
+            CASE
+                WHEN sls_sales IS NULL
+                     OR sls_sales < 0
+                     OR sls_sales <> (sls_quantity * ABS(sls_price))
                 THEN (sls_quantity * ABS(sls_price))
-            ELSE sls_sales
-          END AS sls_sales,
-          sls_quantity,
-          CASE
-            WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales/ NULLIF(sls_quantity,0)
-            ELSE sls_price
-          END AS sls_price
-     FROM DataWarehouse.bronze.crm_sales_details
+                ELSE sls_sales
+            END AS sls_sales,
+            sls_quantity,
+            CASE
+                WHEN sls_price IS NULL OR sls_price <= 0
+                THEN sls_sales / NULLIF(sls_quantity,0)
+                ELSE sls_price
+            END AS sls_price
+        FROM DataWarehouse.bronze.crm_sales_details
+        WHERE TRY_CONVERT(
+                  DATE,
+                  CAST(sls_order_dt AS VARCHAR(8)),
+                  112
+              ) <= CAST(GETDATE() AS DATE)
     );
+
 
     -- 4)
     PRINT '>> Truncating table : silver.erp_cust_az12'
@@ -157,6 +169,7 @@ BEGIN
         END AS gen
     FROM bronze.erp_cust_az12;
 
+
     -- 5)
     PRINT '>> Truncating table : silver.erp_loc_a101'
     TRUNCATE TABLE silver.erp_loc_a101;
@@ -164,18 +177,20 @@ BEGIN
     PRINT '>> Inserting Data into : silver.erp_loc_a101'
     INSERT INTO silver.erp_loc_a101
     (
-	    CID,
-	    CNTRY
+        CID,
+        CNTRY
     )
-	    (SELECT
-		    REPLACE(CID,'-','') AS CID,
-		    CASE
-			    WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
-			    WHEN TRIM(CNTRY) IN ('US','USA') THEN 'United States'
-			    WHEN TRIM(CNTRY) = '' OR TRIM(CNTRY) IS NULL THEN 'n/a'
-			    ELSE TRIM(CNTRY)
-		    END AS CNTRY
-	    FROM bronze.erp_loc_a101);
+    (
+        SELECT
+            REPLACE(CID,'-','') AS CID,
+            CASE
+                WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
+                WHEN TRIM(CNTRY) IN ('US','USA') THEN 'United States'
+                WHEN TRIM(CNTRY) = '' OR TRIM(CNTRY) IS NULL THEN 'n/a'
+                ELSE TRIM(CNTRY)
+            END AS CNTRY
+        FROM bronze.erp_loc_a101
+    );
 
 
     -- 6)
@@ -190,12 +205,12 @@ BEGIN
         SUBCAT,
         MAINTENANCE
     )
-        SELECT
-            ID,
-            CAT,
-            SUBCAT,
-            MAINTENANCE
-        FROM bronze.erp_px_cat_g1v2;
+    SELECT
+        ID,
+        CAT,
+        SUBCAT,
+        MAINTENANCE
+    FROM bronze.erp_px_cat_g1v2;
 
 END;
 GO
